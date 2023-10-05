43ae87190fd2bc2a2985de581a0556ecda865d45
Radoslaw Tomczyk <s93175@pollub.edu.pl>
2023-10-05 15:07:21 +0200

    Added fun1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void fun1(){
-        for (int i=0;i<5;i++){
+        for (int i=0;i<51;i++){
             System.out.println(i);
         }
     }
