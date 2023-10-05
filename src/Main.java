b632b6390b673c9eafdc0bfa376acce3a1681df6
Radoslaw Tomczyk <s93175@pollub.edu.pl>
2023-10-05 15:33:18 +0200

    feat/pollub-2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.nio.file.attribute.UserPrincipal;
+
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,12 +10,22 @@ public class Main {
             System.out.println("Alamakota");
             System.out.println("Alamakota");
             System.out.println("Alamakota");
+            System.out.println("Alamakota");
+            System.out.println("Alamakota");
+            System.out.println("Alamakota");
+            System.out.println("Alamakota");
+            System.out.println("Alamakota");
+            System.out.printf("ashdljhbaskjdh");
         }
     }
+
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        User user = new User();
+        user.showUser();
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
